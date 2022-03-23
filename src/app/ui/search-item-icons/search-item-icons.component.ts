a41f9218fa3050d6f478da394cdc06c017ef157a
eldrssn <maxkalimullin@gmail.com>
2022-03-23 23:31:21 +0300

    init: start youtube-task

---
+++ src/app/ui/search-item-icons/search-item-icons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-item-icons',
+  templateUrl: './search-item-icons.component.html',
+  styleUrls: ['./search-item-icons.component.scss']
+})
+export class SearchItemIconsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
